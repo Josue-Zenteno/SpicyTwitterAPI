283d05058f7d87ede847ada8c73c13bdaf157347
Josue Carlos Zenteno Yave <61275784+Josue-Zenteno@users.noreply.github.com>
2021-03-12 20:30:57 +0100

    app.js V1.1.0

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+require('dotenv').config();
+
+//mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -18,14 +25,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended:true}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() =>  console.log('spicydb connection successful'))
+    .catch((err) => console.error(err));
 
 // error handler
 app.use(function(err, req, res, next) {
